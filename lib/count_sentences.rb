f7534e4e2fef13444138904b520482396d7e931f
Leah Cardoz <leahcardoz@gmail.com>
2022-11-22 11:39:21 -0500

    commit tree 221ba120eaf052ca9f1595389cb213f3db83fb94

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "#{self}".end_with?(".")
   end
 
   def question?
-
+    "#{self}".end_with?("?")
   end
 
   def exclamation?
-
+    "#{self}".end_with?("!")
   end
 
   def count_sentences
-
+    if "#{self}".exclude?("." | "?" | "!")
+      puts "0"
+    else
+    puts "#{self}".split(/\.|!|\?/).count
+    end
   end
+
 end
+
+"Hello friend! Leah? Ok.".count_sentences
